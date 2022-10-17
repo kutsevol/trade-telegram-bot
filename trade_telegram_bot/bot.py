d05c46dbe6d9b069af1448efb6db8fe1c1f32fd6
Artur Kutsevol <arthur.kutsevol@gmail.com>
2022-10-17 18:06:03 +0300

    Move core instances from bot.py
    Create core/manage.py with bot and dispatcher instances.
    Remove these instances from bot.py

--- trade_telegram_bot/bot.py
+++ trade_telegram_bot/bot.py
@@ -1,23 +1,17 @@
 import logging
 
-from aiogram import Bot
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 
 from trade_telegram_bot.core.envs import env_vars
+from trade_telegram_bot.core.manage import bot, dp
 from trade_telegram_bot.handlers.echo import echo
 from trade_telegram_bot.utils.consts import pyproject_toml
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=env_vars.telegram_token)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
-
 
 async def on_startup(dp):
-    logging.info(f"{pyproject_toml['tool']['poetry']['version']}: Set webhook...")
+    logging.info(f"{pyproject_toml['tool']['poetry']['version']}: Set webhook")
     await bot.set_webhook(env_vars.webhook_url, drop_pending_updates=True)
 
 

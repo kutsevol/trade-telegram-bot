4b1653694d4e9c47ca07afd306627c601438cdf8
Artur Kutsevol <arthur.kutsevol@gmail.com>
2022-10-18 18:28:28 +0300

    Split EnvVars in different classes and update .env template
    Create the following classes:
    - BaseVars inherited from BaseSettings and implemented Config class inside
    - DBVars contains all variables for connecting to DB
    - TelegramVars contains all variables for Telegram client
    - EnvVars is a Facade pattern for all other classes
    
    Add all necessary variables to the template:
    - DB_HOST
    - DB_USERNAME
    - DB_PASSWORD
    - DB_NAME

--- trade_telegram_bot/utils/consts.py
+++ trade_telegram_bot/utils/consts.py
@@ -9,21 +9,34 @@ ENV_FILE = ".env"
 PROJECT_FILE = "pyproject.toml"
 
 
-class EnvVars(BaseSettings):
-    telegram_token: str = Field(..., env="TELEGRAM_TOKEN")
-    webhook_host: str = Field(..., env="WEBHOOK_HOST")
-    webapp_host: str = Field(..., env="WEBAPP_HOST")
-    webapp_port: int = Field(..., env="WEBAPP_PORT")
-
-    webhook_url: str | None
-
+class BaseVars(BaseSettings):
     class Config:
         case_sensitive = False
         env_file = BASE_DIR.joinpath(SECRETS_DIR).joinpath(ENV_FILE)
 
+
+class DBVars(BaseVars):
+    host: str = Field(..., env="DB_HOST")
+    user: str = Field(..., env="DB_USERNAME")
+    passwd: str = Field(..., env="DB_PASSWORD")
+    db_name: str = Field(..., env="DB_NAME")
+
+
+class TelegramVars(BaseVars):
+    token: str = Field(..., env="TELEGRAM_TOKEN")
+    webhook_host: str = Field(..., env="WEBHOOK_HOST")
+    webapp_host: str = Field(..., env="WEBAPP_HOST")
+    webapp_port: int = Field(..., env="WEBAPP_PORT")
+    webhook_url: str | None
+
     @validator("webhook_url", always=True)
     def validate_webhook_url(cls, value, values):
-        return f"{values['webhook_host']}/{values['telegram_token']}"
+        return f"{values['webhook_host']}/{values['token']}"
+
+
+class EnvVars:
+    telegram: TelegramVars = TelegramVars()
+    db: DBVars = DBVars()
 
 
 with open(BASE_DIR.parent.joinpath(PROJECT_FILE), "rb") as file:
